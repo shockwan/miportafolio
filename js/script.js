34ad2af42afd4bf5aa029d44081f8ff5142f7adf
pancho <frubilar13@gmail.com>
2019-09-04 23:03:17 -0400

    Se agrega el smooth scrolling quedando perfectamente integrado

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
 
+$(function(){
+
+  $("a").click(function(event){
+    if (this.hash !=="") {
+      event.preventDefault();
+      var gato = this.hash;
+      $("html, body").animate({
+        scrollTop: $(gato).offset().top
+      }, 950, function(){
+        window.location.hash = gato;
+      });
+    }
+
+      });
+
+  });
