fc6a1f61d7becff1c6c938fc058e8955bf072097
pancho <frubilar13@gmail.com>
2019-09-05 19:17:10 -0400

    Se agrega carpeta Assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,20 @@
-$(function () {
+$(function() {
   $('[data-toggle="tooltip"]').tooltip()
 })
 
-$(function(){
+$(function() {
 
-  $("a").click(function(event){
-    if (this.hash !=="") {
+  $("a").click(function(event) {
+    if (this.hash !== "") {
       event.preventDefault();
       var gato = this.hash;
       $("html, body").animate({
         scrollTop: $(gato).offset().top
-      }, 950, function(){
+      }, 950, function() {
         window.location.hash = gato;
       });
     }
 
-      });
-
   });
+
+});
